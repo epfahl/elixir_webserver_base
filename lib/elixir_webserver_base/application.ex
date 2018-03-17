79b93fc30cd698f2c8ae6269e68d8551f27e0c00
Eric Pfahl <eric.pfahl@gmail.com>
2018-03-17 14:52:35 -0400

    add get_port helper function

--- lib/elixir_webserver_base/application.ex
+++ lib/elixir_webserver_base/application.ex
@@ -2,15 +2,16 @@ defmodule ElixirWebserverBase.Application do
   use Application
 
   def start(_type, _args) do
-    port =
-      Application.get_env(:elixir_webserver_base, :http_port)
-      |> String.to_integer()
-
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, ElixirWebserverBase.Router, [], port: port)
+      Plug.Adapters.Cowboy.child_spec(:http, ElixirWebserverBase.Router, [], port: get_port())
     ]
 
     opts = [strategy: :one_for_one, name: ElixirWebserverBase.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp get_port() do
+    Application.get_env(:elixir_webserver_base, :http_port)
+    |> String.to_integer()
   end
 end
