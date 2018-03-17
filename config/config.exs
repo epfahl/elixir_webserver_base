05ca4670d11c80ee2326e6e2806a6da1bb8b369c
Eric Pfahl <eric.pfahl@gmail.com>
2018-03-17 15:45:33 -0400

    moved port string to integer to config.exs

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,5 @@ use Mix.Config
 # Note that the PORT is a string when read from the environment.  The String
 # is converted to an integer in the application module.
 
-config :elixir_webserver_base, http_port: System.get_env("PORT") || "8000"
+port = System.get_env("PORT") || "8000"
+config :elixir_webserver_base, http_port: String.to_integer(port)
