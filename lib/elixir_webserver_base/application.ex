1a1d1aa192a51ecfb0dc7bddfa0fe02037ce06b0
Eric Pfahl <eric.pfahl@gmail.com>
2018-09-30 13:24:51 -0400

    upgrade to cowboy2

--- lib/elixir_webserver_base/application.ex
+++ lib/elixir_webserver_base/application.ex
@@ -5,7 +5,12 @@ defmodule ElixirWebserverBase.Application do
     port = Application.get_env(:elixir_webserver_base, :http_port)
 
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, ElixirWebserverBase.Router, [], port: port)
+      # Plug.Adapters.Cowboy.child_spec(:http, ElixirWebserverBase.Router, [], port: port)
+
+      {
+        Plug.Adapters.Cowboy2,
+        scheme: :http, plug: ElixirWebserverBase.Router, options: [port: port]
+      }
     ]
 
     opts = [strategy: :one_for_one, name: ElixirWebserverBase.Supervisor]
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirWebserverBase.MixProject do
     [
       app: :elixir_webserver_base,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -20,8 +20,8 @@ defmodule ElixirWebserverBase.MixProject do
 
   defp deps do
     [
-      {:plug, "~> 1.5"},
-      {:cowboy, "~> 1.1"}
+      {:plug, "~> 1.6"},
+      {:cowboy, "~> 2.4"}
     ]
   end
 end
